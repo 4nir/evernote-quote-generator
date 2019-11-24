eed74610caed3509c6dd27f4caa1a0ad19992f51
Anirudh Kaushik <anirkkau@usc.edu>
2019-11-24 12:50:40 -0800

    fixed QuoteRenderHooks.js

--- src/components/QuoteRenderHooks.js
+++ src/components/QuoteRenderHooks.js
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 
 const QuoteRenderHook = () => {
   // array that stores quotes
-  const [quotes, setQuotes] = useState([]);
+  const [quotes, setQuotes] = useState(['first', 'second', 'third', 'fourth', 'fifth']);
   // string that stores quote
-  const [quote, setQuote] = useState("first load from hooks");
+  const [quote, setQuote] = useState('first load from hooks');
 
   const generateRandomQuote = () => {
     let new_text = quotes[Math.floor(Math.random() * quotes.length)];
+    console.log('new_text: ', new_text);
     setQuote(new_text);
   };
 
   return (
     <div>
       <div>
-        <p id="text">{quote}</p>
+        <p id='text'>{quote}</p>
         <p id="author">Some author</p>
-        <button onClick={generateRandomQuote} id="new-quote">
+        <button onClick={generateRandomQuote} id='new-quote'>
           New Quote
         </button>
       </div>
